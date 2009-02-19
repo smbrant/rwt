9fe5997da63129a06a1f7064e87badf0debc7cff
sergio <smbrant@gmail.com>
2009-02-19 18:27:07 -0300

    gitignore

--- bck.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/ruby -w
-require 'fileutils'
-puts `zip -r #{Time.now.strftime("%Y_%m_%d__%H_%M")}__#{File.basename(FileUtils.pwd)} * -x nbproject/\\* -x \\*.zip -x \\*.sh* -x log/\\* -x public/dojo/\\* -x public/ext/\\* -x tmp/\\* -x vendor/\\*`
--- bckw.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/ruby -w
-require 'fileutils'
-puts `7z a -tzip #{Time.now.strftime("%Y_%m_%d__%H_%M")}__#{File.basename(FileUtils.pwd)}.zip -r -x!nbproject -x!*.zip -x!*.sh* -x!log -x!public\\dojo -x!public\\ext -x!tmp -x!vendor -x!.git`
